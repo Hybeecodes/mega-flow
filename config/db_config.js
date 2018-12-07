4627c762ab694dd0c44fe433864776d132e5109e
Obikoya Emmanuel <obikoya11@gmail.com>
2018-12-07 12:22:24 +0100

    Update db_config.js

--- config/db_config.js
+++ config/db_config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/megaflow').then(()=>{
+mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds147034.mlab.com:47034/mega-flow`).then(()=>{
     console.log('Connected to database');
 }).catch((err)=>{
     console.log(err);
